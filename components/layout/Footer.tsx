707b0baec6e5f1f3c6b97a993739d7b0effc9388
BeautyKim <kima2012114@gmail.com>
2022-06-28 16:00:37 +0900

    원상복구/스타일깨짐 그대로

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,18 +3,14 @@ import Link from "next/link";
 export const Footer = () => {
     return(
         <footer>
-            <hr/>
-            <div className="container">
-                <Link href="/common/introduce" target='_target'><a>팀소개</a></Link>
-            <p>Copyright ©2022 All rights reserved by Readvice</p>
+            <div className="flex flex-col items-center mt-16">
+                <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
+                    <Link href="/common/introduce" target='_target'><a>팀소개</a></Link>
+                </div>
+                <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+                    <p>Copyright ©2022 All rights reserved by Readvice</p>
+                </div>
             </div>
-            <style jsx>{`
-                .container {
-                    display: flex;
-                    flex-direction: column;
-                    text-align: center;
-                }
-            `}</style>
         </footer>
     )
 }
