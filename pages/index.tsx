d4f460163897f831e637d04533f7f1de6b862c9f
BeautyKim <kima2012114@gmail.com>
2022-07-21 20:02:10 +0900

    2022-07-21 next post test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,10 @@
 import Product from "@/components/common/Product";
-import axios from "axios";
 import { NextPage } from "next";
-import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
-  const [ time, setTime ] = useState('')
-
-  const api = async() => {
-    try {
-      const url = await axios.get('http://127.0.0.1:8000')
-      console.log(JSON.stringify(url))
-      return url.data
-    } catch (err) {
-      console.log(err)
-      return err;
-    }
-  }
-
-  const promise = Promise.resolve(api())
-  useEffect(() => {
-    promise.then((res) => {
-      setTime(res.message)
-      console.log(res)
-    })
-  })
 
   return (
     <>
-      <div>현재시간 :{time}</div>
       <Product />
     </>
     )
--- pages/test.tsx
+++ pages/test.tsx
@@ -1,21 +1,40 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
 
 const test = () => {
     const [ time, setTime ] = useState('')
 
-    const api = async() => {
-      try {
-        const url = await axios.get('http://127.0.0.1:8000')
-        console.log(JSON.stringify(url))
-        return url.data
-      } catch (err) {
-        console.log(err)
-        return err;
-      }
+    const testApi = {
+        receive: async() => {
+            try {
+                const url = await axios.get('http://127.0.0.1:8000')
+                console.log(JSON.stringify(url))
+                return url.data
+              } catch (err) {
+                console.log(err)
+                return err;
+              }
+        },
+        send: async() => {
+            try {
+                const url = await axios.post('http://127.0.0.1:8000', {
+                    params: { message: 'next에서 보내는 post'}
+                })
+                console.log(JSON.stringify(url))
+              } catch (err) {
+                console.log(err)
+                return err;
+              }
+        }
     }
+    useEffect(() => {
+        testApi.send().then((res) => {
+            console.log(res)
+        })
+    })
   
-    const promise = Promise.resolve(api())
+    const promise = Promise.resolve(testApi.receive())
     useEffect(() => {
       promise.then((res) => {
         setTime(res.message)
@@ -25,7 +44,7 @@ const test = () => {
   
     return (
       <>
-        <h1>현재시간: {time}</h1>
+        <h1 className="text-2xl font-bold text-center">현재시간: {time}</h1>
       </>
       )
   }
