3e2cf2cef14eb7542709c7a37cad43a3e0443f7a
BeautyKim <kima2012114@gmail.com>
2022-07-27 19:24:59 +0900

    chatbot input update

--- components/chatbots/Chats.tsx
+++ components/chatbots/Chats.tsx
@@ -46,7 +46,7 @@ const Chats: React.FC<Props> = props => {
             setMessages(temp2);
         }, 1000);
         }
-    }, [props.sendUserResponse, props.botResponse, messages]);
+    }, [props.sendUserResponse, props.botResponse]);
 
     // enable autoscroll after each message
     useEffect(() => {
