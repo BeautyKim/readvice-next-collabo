de288e26338e6efbcf8a94a71276f13eb44b1e23
김아름 <89237300+BeautyKim@users.noreply.github.com>
2023-05-28 13:10:17 +0900

    불필요 코드 제거

--- src/components/users/Login.tsx
+++ src/components/users/Login.tsx
@@ -28,7 +28,7 @@ const Login: React.FC<LoginProps> = ({handleChange, handleSubmit} : LoginProps) 
                                 <input
                                     name="email"
                                     type="email"
-                                    placeholder="Email Addres"
+                                    placeholder="abc@gmail.com"
                                     onChange={handleChange}
                                     className="block w-full px-4 py-3 text-sm border rounded-lg outline-none"/>
                                 <p className="invisible mt-2 text-sm text-pink-600 peer-invalid:visible">
@@ -40,7 +40,7 @@ const Login: React.FC<LoginProps> = ({handleChange, handleSubmit} : LoginProps) 
                                 <input
                                     name="password"
                                     type="password"
-                                    placeholder="Password"
+                                    placeholder="password123"
                                     onChange={handleChange}
                                     className="block w-full px-4 py-3 text-sm border rounded-lg outline-none"/>
                             </label>
@@ -50,10 +50,14 @@ const Login: React.FC<LoginProps> = ({handleChange, handleSubmit} : LoginProps) 
                             <button className="w-64 py-3 text-xl text-white bg-purple-400 rounded-2xl" type="submit">Login</button>
                             <hr className="my-4"/>
                             <Link href="#">
-                                <a className="inline-block text-sm font-bold text-blue-500 align-baseline hover:text-blue-800">Forgot Password?</a></Link>
-                            <p className="mt-4 text-sm">아직 회원이 아니신가요?  
-                                <Link href='/users/join'><a className="underline cursor-pointer">
-                                    Sign Up</a></Link><br/>
+                                <a className="inline-block text-sm font-bold text-blue-500 align-baseline hover:text-blue-800">
+                                    Forgot Password?
+                                </a>
+                            </Link>
+                            <p className="mt-4 text-sm">아직 회원이 아니신가요?
+                                <Link href='/users/join'>
+                                    <a className="underline cursor-pointer">Sign Up</a>
+                                </Link>
                             </p>
                         </div>
                     </div>
